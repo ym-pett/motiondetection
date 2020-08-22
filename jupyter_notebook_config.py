cd66dba067202171b0085756e48df1615bd098e3
Mark Wibrow <m.wibrow@gmail.com>
2020-08-22 15:18:03 +0100

    Name variables consistently

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,7 +1,7 @@
 import os
 
 project_root = os.path.abspath(os.path.dirname(__file__))
-data = os.path.join(project_root, 'data')
+data_root = os.path.join(project_root, 'data')
 
 ipython_dir = os.path.join(project_root, 'notebooks', '.ipython')
 os.environ['IPYTHONDIR'] = ipython_dir
@@ -10,6 +10,6 @@ os.makedirs(startup_dir, exist_ok=True)
 
 with open(os.path.join(startup_dir, '00-startup.py'), 'w') as file_out:
     file_out.write(f'PROJECT_ROOT = "{project_root}"\n')
-    file_out.write(f'DATA_ROOT = "{data}"\n')
+    file_out.write(f'DATA_ROOT = "{data_root}"\n')
 
 c.NotebookApp.notebook_dir = './notebooks' # pylint: disable=undefined-variable
